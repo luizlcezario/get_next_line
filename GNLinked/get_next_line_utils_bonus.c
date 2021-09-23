0b91296e003cfcd0c2f89541e6ff50dae1e7e9f9
luizlcezario <luizlcezario@gmail.com>
2021-09-23 02:47:24 -0300

    add_bonus_linked

---
+++ GNLinked/get_next_line_utils_bonus.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llima-ce <llima-ce@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 17:16:50 by llima-ce          #+#    #+#             */
+/*   Updated: 2021/09/23 02:35:54 by llima-ce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *str, int n)
+{
+	int	a;
+
+	a = 0;
+	while (*(str + a) != (char)n)
+	{
+		if (*(str + a) == 0)
+			return (NULL);
+		a++;
+	}
+	return ((char *)(str + a));
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(1 * sizeof(t_list));
+	if (new == NULL)
+		return (NULL);
+	new->content = content;
+	new->next = (NULL);
+	return (new);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (new == NULL)
+		return ;
+	if (lst[0] == NULL)
+		lst[0] = new;
+	else
+	{
+		tmp = lst[0];
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	a;
+
+	a = 0;
+	while (str[a])
+		a++;
+	return (a);
+}
+
+char	*ft_strdup(const char *str)
+{
+	size_t	i;
+	size_t	len;
+	char	*s;
+
+	len = ft_strlen(str) + 1;
+	s = (char *)malloc(sizeof(char) * len);
+	if (s == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < len)
+		s[i] = str[i];
+	return (s);
+}
